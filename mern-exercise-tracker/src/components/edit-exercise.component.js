4c38a3e1cc6801e25edd7ccd4c48c22a545134a7
st970703 <st970703@users.noreply.github.com>
2021-02-26 18:38:41 +1300

    Completed the create-exercise and create-user features

---
+++ mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class EditExercise extends Component {
+
+  render() {
+    return (
+        <div>
+          <p>YOu are on the Edit Exercise Comp!</p>
+        </div>
+    );
+  }
+}
