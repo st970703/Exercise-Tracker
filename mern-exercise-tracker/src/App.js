01fca16bb83dbae3492cd7b108e752e7903da693
st970703 <st970703@users.noreply.github.com>
2021-02-27 22:38:40 +1300

    Formatted code files and removed unused imports

--- mern-exercise-tracker/src/App.js
+++ mern-exercise-tracker/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
